581b8f80f2569baf032e7d2de5030e5e0a2d693f
CatalinaAguilera <49456246+CatalinaAguilera@users.noreply.github.com>
2019-04-23 16:09:47 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -1,43 +1,135 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+#library(shiny)
+#library(shinyjs)
+library(sp)
+library(raster)
 
-library(shiny)
-library(imager)
-
-shinyServer(function(input, output) {
+function(input, output, session) {
   
-  imageOriginal = NULL
-  imageName = NULL
+  v <- reactiveValues(
+    originalImage = NULL,
+    croppedImage = NULL,
+    imgMask = NULL,
+    imgclick.x = NULL,
+    imgclick.y = NULL,
+    crop.img = FALSE,
+    imageName = NULL
+  )
+  assign("v", v, envir = .GlobalEnv)
   
-  read.image <- function(image.file){
-    im <- load.image(image.file)
-    if(dim(im)[4] > 3){
-      im <- imappend(channels(im, 1:3), 'c')
-    }
-    im
-  }
- 
-  observeEvent(input$GetFile, {
-    imageOriginal = read.image(input$GetFile$datapath)
-    imageName = gsub("(.jpg|.png)","", input$GetFile$name)
-    
-    output$ploteo <- renderPlot({
-      app.plot(imageOriginal)
+  ### Leer una imagen
+  ### Automaticamente mostrar el nombre de dicha imagen
+  observeEvent(input$file1, {
+    v$originalImage <- read.image(input$file1$datapath)
+    v$croppedImage = NULL
+    v$imgMask = NULL
+    v$imgclick.x = NULL
+    v$imgclick.y = NULL
+    v$crop.img = FALSE
+    v$imageName <- gsub("(.jpg|.png)","", input$file1$name)
+    updateTextInput(session, inputId = "imgName", label = NULL, value = v$imageName)
+    output$plot1 <- renderPlot({
+      app.plot(v$originalImage,v$imgclick.x, v$imgclick.y)
     })
+    # Actualizar lista de seleccion multiple, solo cuando se cargue una imagen
+    
+    updateSelectInput(
+      session,
+      "Lista",
+      choices = c("","Begin Crop","Pause Cropping","Reset Cropping","Crop Image")
+    )
     
   })
   
-  app.plot <- function(im){
-    if(is.null(im)){
-      return(NULL)
-    }
-    plot(im)
-  }
+  ### Seleccion multiple 
+  observeEvent(input$imgName, {
+    v$imageName <- input$imgName
+  })
   
-})
+  observeEvent(input$Lista, {
+    if(input$Lista == ""){
+      return()
+    }else if(input$Lista == "Begin Crop"){
+      v$crop.img <- TRUE
+    }else if(input$Lista == "Pause Cropping"){
+      v$crop.img <- FALSE
+    }else if(input$Lista == "Reset Cropping"){
+      v$croppedImage <- NULL
+      v$imgMask <- NULL
+      v$crop.img <- FALSE
+      v$imgclick.x  <- NULL
+      v$imgclick.y <- NULL
+      
+      output$plot1 <- renderPlot({
+        app.plot(v$originalImage,v$imgclick.x, v$imgclick.y)
+      })
+    }else if(input$Lista == "Crop Image"){
+      if(is.null(v$imgclick.x) | is.null(v$imgclick.y)){
+        v$croppedImage <- v$originalImage
+        v$imgMask <- select.points(v$originalImage, v$imgclick.x, v$imgclick.y)
+      }else{
+        v$imgMask <- select.points(v$originalImage, v$imgclick.x, v$imgclick.y)
+        v$croppedImage <-v$imgMask 
+        v$crop.img <- FALSE
+        v$imgclick.x  <- NULL
+        v$imgclick.y <- NULL
+      }
+      output$plot1 <- renderPlot({
+        app.plot(v$croppedImage)
+      })
+    }
+  })
+
+ observeEvent(input$calculateLAI,{
+    
+    if (is.null(v$originalImage)) {
+      updateTextInput(session,inputId = "LAI",label = NULL,value = "")
+    }else{
+      v$croppedImage[v$croppedImage>=150]<-0
+      v$croppedImage[v$croppedImage>0]<-1
+      
+      full<-v$croppedImage
+      
+      full[full>=0]<-1
+      tot<-cellStats(full,'sum')
+      leaf<-cellStats(v$croppedImage,'sum')
+      lai<-leaf/tot
+      
+      updateTextInput(session, inputId = "LAI", label = NULL, value = round(lai,digits = 2))
+    }
+    
+  })
+  
+  ### Seguimiento de los clicks para realizar el poligono
+  observeEvent(input$plot1_click, {
+    
+    if(v$crop.img){
+      v$imgclick.x <- c(v$imgclick.x, round(input$plot1_click$x))
+      v$imgclick.y <- c(v$imgclick.y, round(input$plot1_click$y))
+      assign("imgclick.x", v$imgclick.x, envir = .GlobalEnv)
+      assign("imgclick.y", v$imgclick.y, envir = .GlobalEnv)
+    }
+  })
+  
+  ### Resetear app
+  observeEvent(input$reset, {
+    reset("LAI")
+    updateSelectInput(session,"Lista",choices = "")
+    reset("imgName")
+    reset("file1")
+    v$originalImage = NULL
+    v$croppedImage = NULL
+    v$imgMask = NULL
+    v$imgclick.x = NULL
+    v$imgclick.y = NULL
+    v$crop.img = FALSE
+    v$imageName = NULL
+    
+  })
+  
+  ### Original Image
+  output$plot1 <- renderPlot({
+    app.plot(v$originalImage, v$imgclick.x, v$imgclick.y)
+  })
+  
+ 
+}
--- ui.R
+++ ui.R
@@ -1,38 +1,49 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+library(shinyjs)
 library(shiny)
-library(imager)
+source("helper.R")
 options(shiny.maxRequestSize = 30*1024^2)
 
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Apps LAI"),
-  
-  # Sidebar with a slider input for number of bins 
+fluidPage(
+  titlePanel("Computing LAI"),
+  useShinyjs(),
   sidebarLayout(
     sidebarPanel(
-      ############### INICIO Side Bar Panel
-    
-       tabPanel("Cargar Imagen",
-                fileInput("GetFile","Cargar Archivo: ",accept = c(".png",".jpg")))
-       
-       
-       
+      fileInput("file1", "Choose Image",
+                accept=c(".png",
+                         ".jpg")),
+      textInput("imgName", "Image Name", ""),
+      selectInput("Lista","Seleccione accion:",choices = ""),br(),
+      textInput(inputId ="LAI",
+                label = "LAI(m^2/m^2):",
+                value ="",
+                width = "75px"),
+      actionButton("calculateLAI","Calculate LAI"),br(),br(),br(),
+      div(actionButton("reset","Reset"),align = "right"),br(),
+      HTML('<center><img src="citra.jpg" width="100"></center>')
     ),
-    ############### FIN Side Bar Panel
-    
-    # Show a plot of the generated distribution
     mainPanel(
+      plotOutput("plot1", click="plot1_click",
+                 dblclick = "plot1_dblclick",
+                 brush = brushOpts(
+                   id = "plot1_brush",
+                   resetOnNew = TRUE
+                 )),
       
-      plotOutput("ploteo")
+      column(style='border: 1px solid green',
+             width=12,
+             HTML("<u><h4>Instrucciones: </h4></u>",
+                  "<p>- Antes de realizar una accion debe cargar una imagen.</p>",
+                  "<p>- Presione Crop Begin, para luego seleccionar los vertices del poligono que desea recortar.</p>",
+                  "<p>- Presione Pause Cropping, si desea detener por un instante la seleccion del poligono.</p>",
+                  "<p>- Presione Reset Cropping, si desea limpiar la imagen.</p>",
+                  "<p>- Presione Crop Image, si desea recortar la imagen.</p>",
+                  "<p>- Presione Calculate LAI, para calcular el indice de area foliar.</p>",
+                  "<p>- Presione el boton Reset, si desea resetear la aplicacion.</p>")
+      )
+      
+      
+      
+      
     )
   )
-))
+)
